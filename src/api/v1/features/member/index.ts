788329923219f05e12e64ae880b30a8321c437d7
Dammy Ola <dammyolapaix@gmail.com>
2022-12-21 15:51:19 +0000

    Assigned member full name when adding a new member

--- src/api/v1/features/member/index.ts
+++ src/api/v1/features/member/index.ts
@@ -1,12 +1,14 @@
+/**
+ * Imports
+ */
+
+// Importing Model
 import Member from './Member'
-import {
-  addMemberHandler,
-  deleteMemberHandler,
-  editMemberHandler,
-  getMembersHandler,
-  getSingleMemberByIdHandler,
-} from './member.controllers'
+
+//  Importing Interfaces
 import IMember, { IBaseMember } from './member.interfaces'
+
+// Importing Services
 import {
   addMember,
   deleteMember,
@@ -14,25 +16,46 @@ import {
   getMembers,
   getSingleMemberById,
 } from './member.services'
+
+// Importing Controllers
+import {
+  addMemberHandler,
+  deleteMemberHandler,
+  editMemberHandler,
+  getMembersHandler,
+  getSingleMemberByIdHandler,
+} from './member.controllers'
+
+// Importing Routes
 import memberRoutes from './member.routes'
 
+// Importing Utils
+import { getFullName } from './member.utils'
+
+/**
+ * Exports
+ */
+
+// Exporting Model
+export { Member }
+
+//  Exporting Interfaces
+export { IBaseMember, IMember }
+
+// Exporting Services
+export { addMember, deleteMember, editMember, getMembers, getSingleMemberById }
+
+// Exporting Controllers
 export {
-  // interfaces
-  IBaseMember,
-  IMember,
-  Member,
-  // services
-  addMember,
-  deleteMember,
-  editMember,
-  getMembers,
-  getSingleMemberById,
-  // controllers
   getMembersHandler,
   getSingleMemberByIdHandler,
   addMemberHandler,
   editMemberHandler,
   deleteMemberHandler,
-  // Routes
-  memberRoutes,
 }
+
+// Exporting Routes
+export { memberRoutes }
+
+// Exporting Utils
+export { getFullName }
