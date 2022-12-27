7d76270f3376cedab2a46953e6bfc90f5b9ae04d
Dammy Ola <dammyolapaix@gmail.com>
2022-12-27 13:24:04 +0000

    Added CRUD functionality to special contribution

--- src/api/v1/routes/index.ts
+++ src/api/v1/routes/index.ts
@@ -6,6 +6,7 @@ import { departmentRoutes } from '../features/department'
 import { cellRoutes } from '../features/cell'
 import { WelfareRoutes } from '../features/welfare'
 import { titheRoutes } from '../features/tithe'
+import { specialContributionRoutes } from '../features/specialContribution'
 
 const router = express.Router()
 
@@ -14,6 +15,7 @@ router.use('/api/v1/cells', cellRoutes)
 router.use('/api/v1/churchServices', churchServiceRoutes)
 router.use('/api/v1/departments', departmentRoutes)
 router.use('/api/v1/members', memberRoutes)
+router.use('/api/v1/specialContributions', specialContributionRoutes)
 router.use('/api/v1/tithes', titheRoutes)
 router.use('/api/v1/welfares', WelfareRoutes)
 
