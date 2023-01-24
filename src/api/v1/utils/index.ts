1647db51f75fca818612b496a4ca0e239c900611
Dammy Ola <dammyolapaix@gmail.com>
2023-01-24 20:34:46 +0000

    Added query and pagination utils

--- src/api/v1/utils/index.ts
+++ src/api/v1/utils/index.ts
@@ -3,5 +3,14 @@ import {
   changeToHigherDenomination,
   changeToLowerDenomination,
 } from './changeDenomination.utils'
+import getQueryStr from './query.utils'
+import { getPaginationOptions, getPaginationResult } from './pagination.utils'
 
-export { ErrorResponse, changeToHigherDenomination, changeToLowerDenomination }
+export {
+  ErrorResponse,
+  changeToHigherDenomination,
+  changeToLowerDenomination,
+  getQueryStr,
+  getPaginationOptions,
+  getPaginationResult,
+}
