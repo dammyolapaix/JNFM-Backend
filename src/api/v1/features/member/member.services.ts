f2f9bfae4443d9a3d878670a2c10d92fa1eb22a6
Dammy Ola <dammyolapaix@gmail.com>
2023-01-29 17:58:03 +0000

    Set the default filld to sortby to fullName

--- src/api/v1/features/member/member.services.ts
+++ src/api/v1/features/member/member.services.ts
@@ -86,7 +86,7 @@ export const getMemberQueryResults = async (memberQuery: IMemberQuery) => {
     const sortedBy = memberQuery.sort.split(',').join(' ')
     query = query.sort(sortedBy)
   } else {
-    query = query.sort('-fullName')
+    query = query.sort('fullName')
   }
 
   // Pagination
